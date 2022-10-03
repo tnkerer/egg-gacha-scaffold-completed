eee21874716390cf983ca25182747626089460af
Igor Jacques <igorjcqs@gmail.com>
2022-10-03 00:48:49 -0300

    feat: finished frontend

---
+++ src/app.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import "./app.css";
+
+const App: React.FC = () => {
+  const EGTS = [
+    { image: "blueEgg" },
+    { image: "brownEgg" },
+    { image: "cyanRedEgg" },
+    { image: "darkEgg" },
+    { image: "fullGreenEgg" },
+    { image: "orangeEgg" },
+    { image: "whiteBlackEgg" },
+    { image: "whiteGreenEgg" },
+    { image: "yellowStripedEgg" },
+  ];
+
+  const [slide, setSlide] = useState(0);
+
+  return (
+    <main>
+      <div className="background">
+        <div className="decoration">
+          <img src="/images/decorator.svg" alt="Decoration" />
+        </div>
+        <div className="dragon">
+          <img src="/images/dragon.png" alt="Dragon" />
+        </div>
+      </div>
+      <header>
+        <div className="connect">Connect wallet</div>
+      </header>
+      <div className="app">
+        <div className="wrapper">
+          <div className="infosContainer">
+            <div className="infos">
+              <span>MY EGT TOKENS: 0</span>
+              <span>MY EGGS: 0</span>
+            </div>
+            <div className="infosButton">DROP ME MORE EGT!</div>
+          </div>
+          <div className="mintContainer">
+            <div className="left">
+              {slide === EGTS.length ? (
+                <img src="/images/leftArrowUncolored.svg" alt="Left Arrow" />
+              ) : (
+                <img
+                  src="/images/leftArrowColored.svg"
+                  alt="Left Arrow"
+                  onClick={() => setSlide(slide + 1)}
+                />
+              )}
+            </div>
+
+            <div className="center">
+              <div className="count">
+                {slide}/{EGTS.length}
+              </div>
+              <div className="nft">
+                {slide === 0 ? (
+                  <img src="/images/interrogation.svg" alt="No NFT" />
+                ) : (
+                  <img
+                    src={`/images/Eggs/${EGTS[slide - 1].image}.svg`}
+                    alt="No NFT"
+                  />
+                )}
+              </div>
+              <div className="mintButton">MINT NEW EGG!</div>
+            </div>
+
+            <div className="right">
+              {slide === 0 ? (
+                <img src="/images/rightArrowUncolored.svg" alt="Right Arrow" />
+              ) : (
+                <img
+                  src="/images/rightArrowColored.svg"
+                  alt="Right Arrow"
+                  onClick={() => setSlide(slide - 1)}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,8 @@
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import App from "./app";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <main>
-    <header>
-      <div className="connect">Connect wallet</div>
-    </header>
-    <div className="app">
-      <div className="wrapper">
-        <div className="infosContainer">
-          <div className="infos">
-            <span>MY EGT TOKENS: 0</span>
-            <span>MY EGGS: 0</span>
-          </div>
-          <div className="infosButton">DROP ME MORE EGT!</div>
-        </div>
-        <div className="mintContainer">
-          <div className="left">
-            <img src="/leftArrowColored.svg" alt="Left Arrow" />
-          </div>
 
-          <div className="center">
-            <div className="count">0/0</div>
-            <div className="nft">
-              <img src="/interrogation.svg" alt="No NFT" />
-            </div>
-            <div className="mintButton">DROP ME MORE EGT!</div>
-          </div>
-
-          <div className="right">
-            <img src="/rightArrowColored.svg" alt="Left Arrow" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
-);
+root.render(<App />);
